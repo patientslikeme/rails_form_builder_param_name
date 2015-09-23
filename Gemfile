189dd4e7814cefa184af9f5ce23f6bb6438e4198
Nat Budin <nbudin@patientslikeme.com>
2015-09-23 14:02:27 -0400

    CodeClimate test coverage support

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rails_form_builder_param_name.gemspec
 gemspec
+
+gem "codeclimate-test-reporter", group: :test, require: nil
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rails_form_builder_param_name'
 require 'nokogiri'
